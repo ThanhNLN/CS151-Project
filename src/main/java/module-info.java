b2cb0dff4a000906f73fd4985851c63fe4c60799
ThanhNLN <lengocthanh.nguyen@sjsu.edu>
2023-11-14 08:22:26 -0800

    Add ReadFromJson and Restaurant classes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module com.example.michelinrestaurantguide {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires json.simple;
 
     opens com.example.michelinrestaurantguide to javafx.fxml;
     exports com.example.michelinrestaurantguide;
