0a2c04d8a5f4a8df505c1ac41a8dcc0bda46dd09
anniebluu <anniebluu@protonmail.ch>
2023-11-14 08:52:21 -0800

    Changed json filename and add database file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,8 @@ module com.example.michelinrestaurantguide {
     requires javafx.fxml;
 
     requires org.controlsfx.controls;
+    requires java.sql;
+    requires json.simple;
 
     opens com.example.michelinrestaurantguide to javafx.fxml;
     exports com.example.michelinrestaurantguide;
