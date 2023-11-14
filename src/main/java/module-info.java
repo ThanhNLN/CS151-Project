48043c74d4228ed462305abb410f3812a84750b9
anniebluu <anniebluu@protonmail.ch>
2023-11-14 09:41:28 -0800

    Resolve conflict

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.michelinrestaurantguide {
 
     requires org.controlsfx.controls;
     requires json.simple;
+    requires java.sql;
 
     opens com.example.michelinrestaurantguide to javafx.fxml;
     exports com.example.michelinrestaurantguide;
